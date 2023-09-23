36c3eea2d9361f4c35fb6d4252d0f1130a07b617
Riquelme Magalhães <riquelme.souza@arapiraca.ufal.br>
2023-09-23 12:14:45 -0300

    Atualizações

---
+++ QT/principal.py
@@ -0,0 +1,33 @@
+from PyQt5 import uic, QtWidgets
+import requests
+import time
+import threading
+
+
+def atualiza_dados():
+    while True:
+        try:
+            # Substitua pelo IP correto do seu ESP32
+            resposta = requests.get('http://192.168.31.209/')
+            if resposta.status_code == 200:
+                dados = resposta.text
+                dados_separados = dados.split("e")
+                umidade = dados_separados[0][0:4] + "%"
+                temperatura = dados_separados[1][0:4] + " °C"
+
+                # Atualize os valores na interface gráfica
+                tela.label_6.setText(temperatura)
+                tela.label_7.setText(umidade)
+            else:
+                print("Erro na solicitação HTTP:", resposta.status_code)
+        except Exception as e:
+            print("Erro na solicitação HTTP:", str(e))
+
+        time.sleep(2)
+
+
+app = QtWidgets.QApplication([])
+tela = uic.loadUi("QT/tela_monitor.ui")
+threading.Thread(target=atualiza_dados).start()
+tela.show()
+app.exec()
